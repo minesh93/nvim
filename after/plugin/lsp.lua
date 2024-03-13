9be6e23191e8841ee13b62f0e8ab24913e41b651
Minesh <hello@minesh.io>
2024-03-13 22:04:22 +0000

    More LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls", "tailwindcss", "intelephense", "prismals", "html", "tsserver", "vuels" },
+    ensure_installed = { "lua_ls", "tailwindcss", "intelephense", "prismals", "html", "tsserver", "volar" },
 }
 
 local testlongvar = 10000
@@ -9,24 +9,21 @@ local cmp = require'cmp'
 
 cmp.setup({
     snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-      end,
+        -- Use luasnip for snippets
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
     },
     window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-      ['<C-l>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<C-l>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
@@ -37,36 +34,45 @@ cmp.setup({
     }, {
       { name = 'buffer' },
     })
-  })
+})
 
-  --testlongvar
+--testlongvar
 
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
+        { name = 'path' }
+    },{
+        { name = 'cmdline' }
     })
-  })
+})
 
 
-  -- Set up lspconfig.
-  local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  require('lspconfig')['lua_ls'].setup {
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lsp_lua = require('lspconfig')['lua_ls']
+local lsp_html = require('lspconfig')['html']
+local lsp_vue = require('lspconfig')['volar']
+local lsp_php = require('lspconfig')['intelephense']
+local lsp_ts = require('lspconfig')['tsserver']
+
+lsp_lua.setup {
     capabilities = capabilities
-  }
+}
 
-    require('lspconfig')['vuels'].setup {
+lsp_html.setup {
     capabilities = capabilities
-  }
+}
 
-    require('lspconfig')['tsserver'].setup {
-        capabilities = capabilities
-    }
-
-    require('lspconfig')['html'].setup {
+lsp_vue.setup {
     capabilities = capabilities
-  }
+}
+
+lsp_php.setup {
+    capabilities = capabilities
+}
+
+lsp_ts.setup {
+    capabilities = capabilities
+}
